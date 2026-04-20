3b4a41fae0c7af7efbb1fe6aac36d62a48ebda3e
Phan Đình Thắng <106921106+thangpdgcd@users.noreply.github.com>
2026-04-21 00:56:12 +0700

    delete file no impotant

--- backend/src/support-chat/support-chat.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SupportChatGateway } from './support-chat.gateway';
-
-@Module({
-  providers: [SupportChatGateway],
-})
-export class SupportChatModule {}
-
